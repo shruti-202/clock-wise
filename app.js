f3772aef8e204490d8853ca57840c982fac326b9
Shruti Gawande <110720732+shruti-202@users.noreply.github.com>
2023-01-22 20:24:47 +0530

    commit: Updating final changes

---
+++ app.js
@@ -0,0 +1,29 @@
+const  hourElement=document.querySelector("#hours");
+const minutesElement=document.querySelector("#minutes");
+const secondsElement=document.querySelector("#seconds");
+const ampmElement=document.querySelector("#ampm");
+
+
+function updateClock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM";
+
+    if(h > 12){
+        h= h-12;
+        ampm="PM";
+
+    }
+    h= h <10 ?"0"+ h: h;
+    m= m <10 ?"0"+ m: m;
+    s= s <10 ?"0"+ s: s;
+hourElement.innerText= h;
+minutesElement.innerText= m;
+secondsElement.innerText= s;
+ampmElement.innerText=ampm;
+setTimeout(()=>{
+    updateClock()
+},1000)
+}
+updateClock()
